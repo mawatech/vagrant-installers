97a71a33fdada040c5bbcefafae429f0abb5015d
Markus Wagner <markus.wagner@open-xchange.com>
2016-11-15 16:21:58 +0100

    set lib_version

--- substrate/modules/readline/manifests/init.pp
+++ substrate/modules/readline/manifests/init.pp
@@ -16,7 +16,7 @@ class readline(
   $source_dir_name  = regsubst($source_filename, '^(.+?)\.tar\.gz$', '\1')
   $source_dir_path  = "${file_cache_dir}/${source_dir_name}"
 
-  $lib_version = "6"
+  $lib_version = "7"
 
   # Determine if we have an extra environmental variables we need to set
   # based on the operating system.
